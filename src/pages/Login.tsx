75765b92a305d9ca2ff8db8dd06416201d48cf53
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2023-06-20 17:23:43 -0300

    minor improvements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import "./Login.css";
 import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import "./Login.css";
 
 export default function Login() {
   const [username, setUsername] = useState("");
-  const [activeButton, setActiveButton] = useState(false);
+  const [activeButton, setActiveButton] = useState(true);
 
   useEffect(() => {
-    if (username.length > 0) {
+    if (username.length) {
       setActiveButton(false);
-    } else {
-      setActiveButton(true);
+      return;
     }
-  },[username]);
+    setActiveButton(true);
+  }, [username]);
 
   return (
-    <Grid container component="main" sx={{ display:"flex", justifyContent:"center", alignItems:"center" }}>
+    <Grid
+      container
+      component="main"
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
       <Box
         sx={{
           display: "flex",
@@ -33,17 +35,69 @@ export default function Login() {
           borderRadius: "16px",
         }}
       >
-        <div style={{ display: "flex", alignItems: "start", flexDirection: "column", width:"452px", paddingInline: "50px", gap:"24px"}}>
-          <Typography component="span" sx={{ fontSize: "22px", color: "black", fontFamily: "Roboto", fontWeight: "700", lineHeight: "26px"}}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "start",
+            flexDirection: "column",
+            width: "452px",
+            paddingInline: "50px",
+            gap: "24px",
+          }}
+        >
+          <Typography
+            component="span"
+            sx={{
+              fontSize: "22px",
+              color: "black",
+              fontFamily: "Roboto",
+              fontWeight: "700",
+              lineHeight: "26px",
+            }}
+          >
             Welcome to CodeLeap network!
           </Typography>
-          <Typography component="span" sx={{ mb: "4px", fontSize: "16px", color: "black", fontFamily: "Roboto", fontWeight: "400"}}>
+          <Typography
+            component="span"
+            sx={{
+              mb: "4px",
+              fontSize: "16px",
+              color: "black",
+              fontFamily: "Roboto",
+              fontWeight: "400",
+            }}
+          >
             Please enter your username
           </Typography>
         </div>
-        <div style={{ display: "flex", alignItems: "end", flexDirection: "column", gap:"16px"}}>
-          <input onChange={(e)=> setUsername(e.target.value)} placeholder="John Doe" style={{ width: "452px", height: "32px", borderRadius: "8px", background:"#FFFFFF", color:"black", border:"1px solid #777777", fontFamily: "Roboto"}}/>
-          <Button sx={{ width: "111px", height: "32px", borderRadius:"8px" }} variant="contained" disabled={activeButton}>Enter</Button>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "end",
+            flexDirection: "column",
+            gap: "16px",
+          }}
+        >
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="John Doe"
+            style={{
+              width: "452px",
+              height: "32px",
+              borderRadius: "8px",
+              background: "#FFFFFF",
+              color: "black",
+              border: "1px solid #777777",
+              fontFamily: "Roboto",
+            }}
+          />
+          <Button
+            sx={{ width: "111px", height: "32px", borderRadius: "8px" }}
+            variant="contained"
+            disabled={activeButton}
+          >
+            Enter
+          </Button>
         </div>
       </Box>
     </Grid>
